6d7ce05f008c569a0b6738a8dd6de297bb96d9b6
julesfll <45245651+julesfll@users.noreply.github.com>
2020-02-25 17:26:23 -0500

    Updates

---
+++ precache-manifest.74a347d8bb554916e65d568751ae136a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce0172e2a818b45dcd726aca10190e70",
+    "url": "/take-five/index.html"
+  },
+  {
+    "revision": "b15304d941f792730d26",
+    "url": "/take-five/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f84abd917b2d3acaa68a",
+    "url": "/take-five/static/js/2.d9b4464d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/take-five/static/js/2.d9b4464d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b15304d941f792730d26",
+    "url": "/take-five/static/js/main.9da0f0c5.chunk.js"
+  },
+  {
+    "revision": "9d3773c924d5db227354",
+    "url": "/take-five/static/js/runtime-main.b3427101.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/take-five/static/media/logo.5d5d9eef.svg"
+  }
+]);
